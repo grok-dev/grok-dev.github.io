a33fb89c6b45a6fae0aa7678e55af98bdd397225
grok-dev <cav3m4nuo@gmail.com>
2023-04-02 16:33:49 -0700

    move old files to 'old files' and add new overhauled portfolio w/ placeholder content

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var prevScrollpos = window.pageYOffset;
+const arrowPos = document.getElementById("scroll-down-arrow").style.bottom;
+
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
 
@@ -8,4 +10,11 @@ window.onscroll = function () {
     document.getElementById("header").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
+
+  // you're at the bottom of the page
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    document.getElementById("scroll-down-arrow").style.bottom = "-100px";
+  } else if (!document.getElementById("scroll-down-arrow").style.bottom == arrowPos) {
+    document.getElementById("scroll-down-arrow").style.bottom = arrowPos;
+  }
 };
